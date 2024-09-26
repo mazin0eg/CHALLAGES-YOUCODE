8f7304d835bf56fb5893ab177adb3093061abfc3
mazine zougui <mazine.zougui@gmail.com>
2024-09-26 12:11:34 +0100

    challage boucle

---
+++ boucle1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,div,i;
+
+    printf("entrer un nomber de 1 a 10 : ");
+    scanf("%d",&num);
+
+ for(i=1;i<=10;i++){
+    div= i * num;
+    printf(" %d * %d = %d \n",num,i,div);
+}
+    return 0;
+}
