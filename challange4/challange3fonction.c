4fcdf85098125b78d6847f3ec230ab293ef270fa
mazine zougui <mazine.zougui@gmail.com>
2024-09-30 16:40:12 +0100

    challanges

---
+++ challange4/challange3fonction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int max(int a,int b){
+   if(a>b) return a;
+   else return b;
+}
+int main() {
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+int r=max(a,b);
+    printf("\t%d est le plus grande valeur",r);
+    return 0;
+}
