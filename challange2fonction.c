58e17fef76d3126bba10d8e041e1edd3e7dd05ea
mazine zougui <mazine.zougui@gmail.com>
2024-09-30 12:25:47 +0100

    challanges

---
+++ challange2fonction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void sum(int a,int b){
+    printf("%d et %d egale %d",a,b,a*b);
+}
+int main(){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    sum(a,b);
+  return 0;
+}
